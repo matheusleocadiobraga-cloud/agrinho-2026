d4ee08454612dca83a5efd7e8e2d091d7eb25338
matheusleocadiobraga-cloud <matheus.leocadio.braga@escola.pr.gov.br>
2026-06-27 18:09:29 -0300

    agrinho

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function entrar() {
 
     alert(`Bem-vindo, ${nome}! Você tem ${idade} anos.`);
 }
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function () {
+
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    });
+});
+
+
+
+
+
+
+
